feebda845b41140e6c69ff5d80588bc925286c20
Ahmad Chaiban <ahmadchaiban@gmail.com>
2020-04-18 15:47:56 +0300

    Feat (indicator line)
    Added a line that follows the user's mouse, or can be dragged. This line will eventually be able to indicate the y coordinates of the four lines intersecting with it.

--- lineGraphDaily.js
+++ lineGraphDaily.js
@@ -1,261 +1,261 @@
-var svg = d3.selectAll("#lineNode"),
-    margin = {top: 120, right: 50, bottom: 110, left: 100},
-    margin2 = {top: 430, right: 20, bottom: 30, left: 40},
-    width = document.getElementById('lineDiv').offsetWidth - margin.left - margin.right,
-    height = document.getElementById('lineDiv').offsetHeight - margin.top - margin.bottom,
-    height2 = document.getElementById('lineDiv').offsetHeight - margin2.top - margin2.bottom;
+function lineGraph(){
 
-var parseDate = d3.timeParse("%Y-%m-%d");
+    var svg = d3.selectAll("#lineNode"),
+        margin = {top: 120, right: 50, bottom: 110, left: 100},
+        margin2 = {top: 430, right: 20, bottom: 30, left: 40},
+        width = document.getElementById('lineDiv').offsetWidth - margin.left - margin.right,
+        height = document.getElementById('lineDiv').offsetHeight - margin.top - margin.bottom,
+        height2 = document.getElementById('lineDiv').offsetHeight - margin2.top - margin2.bottom;
 
-var x = d3.scaleTime().range([0, width]),
-    x2 = d3.scaleTime().range([0, width]),
-    y = d3.scaleLinear().range([height, 0]),
-    y2 = d3.scaleLinear().range([height2, 0]);
+    svg.selectAll('*').remove()
 
-var xAxis = d3.axisBottom(x),
-    xAxis2 = d3.axisBottom(x2),
-    yAxis = d3.axisLeft(y);
+    var parseDate = d3.timeParse("%Y-%m-%d");
 
-var brush = d3.brushX()
-    .extent([[0, 0], [width, height2]])
-    .on("brush end", brushed);
+    var x = d3.scaleTime().range([0, width]),
+        x2 = d3.scaleTime().range([0, width]),
+        y = d3.scaleLinear().range([height, 0]),
+        y2 = d3.scaleLinear().range([height2, 0]);
 
-var zoom = d3.zoom()
-    .scaleExtent([1, Infinity])
-    .translateExtent([[0, 0], [width, height]])
-    .extent([[0, 0], [width, height]])
-    .on("zoom", zoomed);
+    var xAxis = d3.axisBottom(x),
+        xAxis2 = d3.axisBottom(x2),
+        yAxis = d3.axisLeft(y);
 
-    var line = d3.line()
-        .x(function (d) { return x(d.date); })
-        .y(function (d) { return y(parseInt(d.confirmed)); });
+    var brush = d3.brushX()
+        .extent([[0, 0], [width, height2]])
+        .on("brush end", brushed);
 
-    var line_recovered = d3.line()
-        .x(function (d) { return x(d.date); })
-        .y(function (d) { return y(parseInt(d.recovered)); });
+    var zoom = d3.zoom()
+        .scaleExtent([1, Infinity])
+        .translateExtent([[0, 0], [width, height]])
+        .extent([[0, 0], [width, height]])
+        .on("zoom", zoomed);
 
-    var line_active = d3.line()
-        .x(function (d) { return x(d.date); })
-        .y(function (d) { return y(parseInt(d.active)); });
+        var line = d3.line()
+            .x(function (d) { return x(d.date); })
+            .y(function (d) { return y(parseInt(d.confirmed)); });
 
-    var line_deaths = d3.line()
-        .x(function (d) { return x(d.date); })
-        .y(function (d) { return y(parseInt(d.deaths)); });
+        var line_recovered = d3.line()
+            .x(function (d) { return x(d.date); })
+            .y(function (d) { return y(parseInt(d.recovered)); });
 
-    var clip = svg.append("defs").append("svg:clipPath")
-        .attr("id", "clip")
-        .append("svg:rect")
+        var line_active = d3.line()
+            .x(function (d) { return x(d.date); })
+            .y(function (d) { return y(parseInt(d.active)); });
+
+        var line_deaths = d3.line()
+            .x(function (d) { return x(d.date); })
+            .y(function (d) { return y(parseInt(d.deaths)); });
+
+        var clip = svg.append("defs").append("svg:clipPath")
+            .attr("id", "clip")
+            .append("svg:rect")
+            .attr("width", width)
+            .attr("height", height)
+            .attr("x", 0)
+            .attr("y", 0); 
+
+
+        var Line_chart = svg.append("g")
+            .attr("class", "focus")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+            .attr("clip-path", "url(#clip)");
+
+
+        var focus = svg.append("g")
+            .attr("class", "focus")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    var context = svg.append("g")
+        .attr("class", "context")
+        .attr("transform", "translate(" + margin2.left + "," + margin2.top + ")");
+
+    d3.csv("cases_per_day_agg.csv", type, function (error, data) {
+    if (error) throw error;
+    x.domain(d3.extent(data, function(d) { return d.date; }));
+    y.domain([0, d3.max(data, function (d) { return parseInt(d.confirmed); })]);
+    x2.domain(x.domain());
+    y2.domain(y.domain());
+
+        focus.append("g")
+            .attr("class", "axis axis--x")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis)
+            .style('stroke', 'white');
+
+        focus.append("g")
+            .attr("class", "axis axis--y")
+            .call(yAxis)
+            .style('stroke', 'white');
+
+        Line_chart.append("path")
+            .datum(data)
+            .attr("class", "line")
+            .attr("d", line)
+            .style('stroke','white');
+
+        Line_chart.append("path")
+            .datum(data)
+            .attr("class", "line_recovered")
+            .attr("d", line_recovered)
+            .style('stroke', '#EAD8BD');
+
+        Line_chart.append("path")
+            .datum(data)
+            .attr("class", "line_active")
+            .attr("d", line_active)
+            .style('stroke','#5A8895');
+
+        Line_chart.append("path")
+            .datum(data)
+            .attr("class", "line_deaths")
+            .attr("d", line_deaths)
+            .style('stroke', '#0E77B4');
+
+
+
+    //     context.append("path")
+    //         .datum(data)
+    //         .attr("class", "line")
+    //         .attr("d", line2);
+
+
+    //   context.append("g")
+    //       .attr("class", "axis axis--x")
+    //       .attr("transform", "translate(0," + height2 + ")")
+    //       .call(xAxis2);
+
+    //   context.append("g")
+    //       .attr("class", "brush")
+    //       .call(brush)
+    //       .call(brush.move, x.range());
+
+    svg.append("rect")
+        .attr("class", "zoom")
         .attr("width", width)
         .attr("height", height)
-        .attr("x", 0)
-        .attr("y", 0); 
-
-
-    var Line_chart = svg.append("g")
-        .attr("class", "focus")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-        .attr("clip-path", "url(#clip)");
+        .call(zoom);
 
 
-    var focus = svg.append("g")
-        .attr("class", "focus")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        var columns = ['Confirmed', 'Active', 'Recovered', 'Deaths']
 
-var context = svg.append("g")
-    .attr("class", "context")
-    .attr("transform", "translate(" + margin2.left + "," + margin2.top + ")");
+        // svg.append('rect')
+        //     .style('stroke','white')
+        //     .style('fill','grey')
+        //     .attr('width', 100)
+        //     .attr('height',100)
+        //     .attr('x', 10)
+        //     .attr('y', 10)
+        //     .attr("transform","translate(100,100)");
 
-d3.csv("cases_per_day_agg.csv", type, function (error, data) {
-  if (error) throw error;
-  x.domain(d3.extent(data, function(d) { return d.date; }));
-  y.domain([0, d3.max(data, function (d) { return parseInt(d.confirmed); })]);
-  x2.domain(x.domain());
-  y2.domain(y.domain());
+        svg.selectAll('#lineNode')
+            .append('g')
+            .data(columns)
+            .enter()
+            .append('text')
+            .text(function(d,i){
+                return columns[i];
+            })
+            .style('fill','white')
+            .attr('x',200)
+            .attr('y', function(d,i){
+                return 25*i + 130
+            });
 
-    focus.append("g")
-        .attr("class", "axis axis--x")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis)
-        .style('stroke', 'white');
+        var colors = ['white', '#EAD8BD', '#5A8895', '#0E77B4'];
 
-    focus.append("g")
-        .attr("class", "axis axis--y")
-        .call(yAxis)
-        .style('stroke', 'white');
-
-    Line_chart.append("path")
-        .datum(data)
-        .attr("class", "line")
-        .attr("d", line)
-        .style('stroke','white');
-
-    Line_chart.append("path")
-        .datum(data)
-        .attr("class", "line_recovered")
-        .attr("d", line_recovered)
-        .style('stroke', '#EAD8BD');
-
-    Line_chart.append("path")
-        .datum(data)
-        .attr("class", "line_active")
-        .attr("d", line_active)
-        .style('stroke','#5A8895');
-
-    Line_chart.append("path")
-        .datum(data)
-        .attr("class", "line_deaths")
-        .attr("d", line_deaths)
-        .style('stroke', '#0E77B4');
-
-    var bisect = d3.bisector(function(d) { return d.x; }).left;
-
-        // Create the circle that travels along the curve of chart
-        // var focus = svg.append('g')
-        //   .append('circle')
-        //     .style("fill", "none")
-        //     .attr("stroke", "black")
-        //     .attr('r', 8.5)
-        //     .style("opacity", 0)
-      
-        // Create the text that travels along the curve of chart
-        var focusText = svg.append('g')
-          .append('text')
-            .style("opacity", 0)
-            .attr("text-anchor", "left")
-            .attr("alignment-baseline", "middle")
-      
-        // Create a rect on top of the svg area: this rectangle recovers mouse position
-        svg.append('rect')
-          .style("fill", "none")
-          .style("pointer-events", "all")
-          .attr('width', width)
-          .attr('height', height)
-          .on('mouseover', mouseover)
-          .on('mousemove', mousemove)
-          .on('mouseout', mouseout);
-
-//     context.append("path")
-//         .datum(data)
-//         .attr("class", "line")
-//         .attr("d", line2);
+        
+        svg.selectAll('#lineNode')
+            .append('g')
+            .data(colors)
+            .enter()
+            .append('line')
+            .attr('x1', 180)
+            .attr('x2', 160)
+            .attr('y1',function(d,i){
+                return 25*i +127;
+            })
+            .attr('y2',function(d,i){
+                return 25*i +127;
+            })
+            .style('stroke', function(d,i){
+                return colors[i];
+            })
+            .style('stroke-width',2);
 
 
-//   context.append("g")
-//       .attr("class", "axis axis--x")
-//       .attr("transform", "translate(0," + height2 + ")")
-//       .call(xAxis2);
+        Line_chart.append('line')
+            .attr('class', 'line_indicator')
+            .attr('x1', 0)
+            .attr('x2', 0)
+            .attr('y1', 0)
+            .attr('y2', height)
+            .style('stroke', 'white'); 
 
-//   context.append("g")
-//       .attr("class", "brush")
-//       .call(brush)
-//       .call(brush.move, x.range());
+        d3.selectAll('#lineNode')          
+            .on('mousemove', function(d){
+                d3.selectAll('.line_indicator')
+                    .attr('transform', 'translate(' + d3.mouse(this)[0] + ',' + 0 + ')');
+            })
+            // .on('click', function(d){
+            //     d3.selectAll('.line_indicator')
+            //         .attr('transform', 'translate(' + d3.mouse(this)[0] + ',' + 0 + ')');
+            // })
+            .call(drag);
 
-  svg.append("rect")
-      .attr("class", "zoom")
-      .attr("width", width)
-      .attr("height", height)
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-      .call(zoom);
+    });
 
 
-      var columns = ['Confirmed', 'Active', 'Recovered', 'Deaths']
-
-      // svg.append('rect')
-      //     .style('stroke','white')
-      //     .style('fill','grey')
-      //     .attr('width', 100)
-      //     .attr('height',100)
-      //     .attr('x', 10)
-      //     .attr('y', 10)
-      //     .attr("transform","translate(100,100)");
-
-      svg.selectAll('#lineNode')
-          .append('g')
-          .data(columns)
-          .enter()
-          .append('text')
-          .text(function(d,i){
-              return columns[i];
-          })
-          .style('fill','white')
-          .attr('x',200)
-          .attr('y', function(d,i){
-              return 25*i + 130
-          });
-
-      var colors = ['white', '#EAD8BD', '#5A8895', '#0E77B4'];
-
-      
-      svg.selectAll('#lineNode')
-          .append('g')
-          .data(colors)
-          .enter()
-          .append('line')
-          .attr('x1', 180)
-          .attr('x2', 160)
-          .attr('y1',function(d,i){
-              return 25*i +127;
-          })
-          .attr('y2',function(d,i){
-              return 25*i +127;
-          })
-          .style('stroke', function(d,i){
-              return colors[i];
-          })
-          .style('stroke-width',2);
-});
-
-function brushed() {
-  if (d3.event.sourceEvent && d3.event.sourceEvent.type === "zoom") return; // ignore brush-by-zoom
-  var s = d3.event.selection || x2.range();
-  x.domain(s.map(x2.invert, x2));
-  Line_chart.select(".line").attr("d", line);
-  focus.select(".axis--x").call(xAxis);
-  svg.select(".zoom").call(zoom.transform, d3.zoomIdentity
-      .scale(width / (s[1] - s[0]))
-      .translate(-s[0], 0));
-}
-
-function zoomed() {
-  if (d3.event.sourceEvent && d3.event.sourceEvent.type === "brush") return; // ignore zoom-by-brush
-  var t = d3.event.transform;
-  x.domain(t.rescaleX(x2).domain());
-  Line_chart.select(".line").attr("d", line);
-  Line_chart.select(".line_recovered").attr("d", line_recovered);
-  Line_chart.select(".line_active").attr("d", line_active);
-  Line_chart.select(".line_deaths").attr("d", line_deaths);
-  focus.select(".axis--x").call(xAxis);
-  context.select(".brush").call(brush.move, x.range().map(t.invertX, t));
-}
-
-function type(d) {
-  d.date = parseDate(d.date);
-  d.confirmed = parseInt(d.confirmed);
-  d.recovered = parseInt(d.recovered);
-  d.active = parseInt(d.active);
-  d.deaths = parseInt(d.deaths);
-  return d;
-}
-
-function mouseover() {
-    focus.style("opacity", 1)
-    focusText.style("opacity",1)
-  }
-
-  function mousemove() {
-    // recover coordinate we need
-    var x0 = x.invert(d3.mouse(this)[0]);
-    var i = bisect(data, x0, 1);
-    selectedData = data[i]
-    focus
-      .attr("cx", x(selectedData.x))
-      .attr("cy", y(selectedData.y))
-    focusText
-      .html("x:" + selectedData.x + "  -  " + "y:" + selectedData.y)
-      .attr("x", x(selectedData.x)+15)
-      .attr("y", y(selectedData.y))
+    function brushed() {
+    if (d3.event.sourceEvent && d3.event.sourceEvent.type === "zoom") return; // ignore brush-by-zoom
+    var s = d3.event.selection || x2.range();
+    x.domain(s.map(x2.invert, x2));
+    Line_chart.select(".line").attr("d", line);
+    focus.select(".axis--x").call(xAxis);
+    svg.select(".zoom").call(zoom.transform, d3.zoomIdentity
+        .scale(width / (s[1] - s[0]))
+        .translate(-s[0], 0));
     }
-  function mouseout() {
-    focus.style("opacity", 0)
-    focusText.style("opacity", 0)
+
+    function zoomed() {
+    if (d3.event.sourceEvent && d3.event.sourceEvent.type === "brush") return; // ignore zoom-by-brush
+    var t = d3.event.transform;
+    x.domain(t.rescaleX(x2).domain());
+    Line_chart.select(".line").attr("d", line);
+    Line_chart.select(".line_recovered").attr("d", line_recovered);
+    Line_chart.select(".line_active").attr("d", line_active);
+    Line_chart.select(".line_deaths").attr("d", line_deaths);
+    focus.select(".axis--x").call(xAxis);
+    context.select(".brush").call(brush.move, x.range().map(t.invertX, t));
+    }
+
+    function type(d) {
+    d.date = parseDate(d.date);
+    d.confirmed = parseInt(d.confirmed);
+    d.recovered = parseInt(d.recovered);
+    d.active = parseInt(d.active);
+    d.deaths = parseInt(d.deaths);
+    return d;
+    }
+
+var drag = d3.drag()
+    .on("start", dragstarted)
+    .on("drag", dragged)
+    .on("end", dragended);
+
+function dragstarted(d) {
+    d3.selectAll('.line_indicator').style("stroke", "white");
   }
+
+  function dragged(d) {
+
+    d3.selectAll('.line_indicator')
+                    .attr('transform', 'translate(' + d3.event.x + ',' + 0 + ')')
+
+    // d3.select(this).raise().attr("cx", d.x = d3.event.x).attr("cy", d.y = d3.event.y);
+  }
+
+  function dragended(d) {
+    d3.selectAll('.line_indicator').attr("stroke", 'white');
+  }
+}
